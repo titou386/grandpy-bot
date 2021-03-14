2da1bb427ffbd6fddd88111bc413b7b932f0f511
Yves <yves.dornier@yahoo.fr>
2021-03-14 12:33:04 +0100

    Suggested improvements

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,44 +1,29 @@
 """Test_parser.py."""
 from app.parser import Parser
+import pytest
 
 
 class TestParser:
     """Test Parser class."""
 
-    def test_find_location_1(self):
-        """First test of find_location method."""
-        p = Parser()
-        location = p.find_location("""
-Hey GrandPy, indique moi où se trouve la poste ?""")
-        assert location == ['la', 'poste']
-
-    def test_find_location_2(self):
-        """Second test of find_location method."""
-        p = Parser()
-        location = p.find_location("""
-Salut, indique moi le bureau de tabac.""")
-        assert location == ['le', 'bureau', 'de', 'tabac']
-
-    def test_find_location_3(self):
-        """Third test of find_location method."""
-        p = Parser()
-        location = p.find_location("""
-Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?""")
-        assert location == ['openclassrooms']
-
-    def test_find_location_4(self):
-        """Attack attempt."""
-        p = Parser()
-        location = p.find_location("""
+    @pytest.mark.parametrize("test_input,expected",
+                             [("""
+Hey GrandPy, indique moi où se trouve la poste ?""",
+                              ['la', 'poste']),
+                              ("""
+Salut, indique moi le bureau de tabac.""",
+                              ['le', 'bureau', 'de', 'tabac']),
+                              ("""
+Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?""",
+                              ['openclassrooms']),
+                              ("""
 Salut GrandPy ! Est-ce que tu connais l'adresse\
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" \
 integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" \
-crossorigin="anonymous"></script>d'OpenClassrooms ?""")
-        assert location is None
-
-    def test_find_location_5(self):
-        """Fifth test of find_location method."""
+crossorigin="anonymous"></script>d'OpenClassrooms ?""", None),
+                              ("Hey, ça va ?", None)])
+    def test_find_location(self, test_input, expected):
+        """First test of find_location method."""
         p = Parser()
-        location = p.find_location("""
-Hey, ça va ?""")
-        assert location is None
+        location = p.find_location(test_input)
+        assert location == expected
